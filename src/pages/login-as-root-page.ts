5ca5bff47613159d2a9460ebbf3772f01d2c6b5e
jknphy <jeriveramoya@suse.com>
2024-09-12 14:01:02 +0200

    Store page properties as fat arrow function

--- src/pages/login-as-root-page.ts
+++ src/pages/login-as-root-page.ts
@@ -2,17 +2,17 @@ import puppeteer, { type Locator, type Page } from "puppeteer-core";
 
 export class LoginAsRootPage {
     readonly page: Page;
-    readonly passwordInput: Locator<Element>;
-    readonly logInButton: Locator<Element>;
+    readonly passwordInput;
+    readonly logInButton;
 
     constructor(page: Page) {
         this.page = page;
-        this.passwordInput = page.locator("input#password");
-        this.logInButton = page.locator("button[type='submit']");
+        this.passwordInput = () => page.locator("input#password");
+        this.logInButton = () => page.locator("button[type='submit']");
     }
 
     async logIn(password: string) {
-        await this.passwordInput.fill(password);
-        await this.logInButton.click();
+        await this.passwordInput().fill(password);
+        await this.logInButton().click();
     }
 }
