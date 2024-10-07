d71b6bc78be894403a12e73d28ee55c1a8bb4c80
Ladislav Slezák <lslezak@suse.com>
2024-10-07 14:44:37 +0200

    Compile all tests, use chunks

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,10 +2,17 @@ const path = require("path");
 const fs = require("fs");
 const webpack = require("webpack");
 
+const entry = {}
+
+// process all ./src/test_*.ts files
+fs.readdirSync("./src").filter(f => f.startsWith("test_") && f.endsWith(".ts")).forEach(
+  (f) => { entry[path.basename(f, ".ts")] = "./src/" + f }
+);
+
 module.exports = {
-  entry: "./src/test_root_password.ts",
+  entry,
   output: {
-    filename: "test_root_password.cjs",
+    filename: "[name].cjs",
     path: path.resolve(__dirname, "dist"),
     clean: true
   },
@@ -25,18 +32,19 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
   plugins: [
-    // do not split the code into several files, generate a single output file
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1,
-    }),
     // prepend a hashbang at the beginning of the generated file
-    new webpack.BannerPlugin({ banner: "#! /usr/bin/env -S node --test-timeout=60000", raw: true }),
+    new webpack.BannerPlugin({ banner: "#! /usr/bin/env -S node --test-timeout=60000", raw: true, test: /^test_.*\.cjs$/ }),
     // make the file JS files executable
     function() {
       this.hooks.done.tap("Change permissions", (data) => {
         Object.keys(data.compilation.assets).forEach((file) => {
-          if (file.match(/\.cjs$/)) {
+          if (file.match(/^test_.*\.cjs$/)) {
             fs.chmodSync(`${__dirname}/dist/${file}`, 0o755);
           }
         });
