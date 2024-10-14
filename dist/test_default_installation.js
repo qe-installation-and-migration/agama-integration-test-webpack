c9289b5053525144a24f0206078232238a994b76
jknphy <jeriveramoya@suse.com>
2024-10-14 08:29:21 +0200

    Fix product selection

--- dist/test_default_installation.js
+++ dist/test_default_installation.js
@@ -136,18 +136,11 @@ function productSelection(product) {
     (0, helpers_1.it)("should display the product selection dialog", async function () {
         const productselection = new product_selection_page_1.ProductSelectionPage(helpers_1.page);
         let timeout = 2 * 60 * 1000;
-        if (product !== "sle") {
-            if (product === "leap") {
-                await productselection.selectProduct("Leap 16.0 Alpha");
-            }
-            else {
-                await productselection.selectProduct("openSUSE Tumbleweed");
-            }
-            // Check if configuration procedure is progressing
-            await helpers_1.page.locator("::-p-text(Configuring the product)").wait();
-            // refreshing the repositories might take long time
-            await helpers_1.page.locator("h3::-p-text('Overview')").setTimeout(timeout).wait();
-        }
+        await productselection.selectProduct(product);
+        // Check if configuration procedure is progressing
+        await helpers_1.page.locator("::-p-text(Configuring the product)").wait();
+        // refreshing the repositories might take long time
+        await helpers_1.page.locator("h3::-p-text('Overview')").setTimeout(timeout).wait();
     });
 }
 
--- src/checks/product_selection.ts
+++ src/checks/product_selection.ts
@@ -6,18 +6,11 @@ export function productSelection(product: string) {
         const productselection = new ProductSelectionPage(page);
         let timeout = 2 * 60 * 1000;
 
-        if (product !== "sle") {
-            if (product === "leap") {
-                await productselection.selectProduct("Leap 16.0 Alpha");
-            }
-            else {
-                await productselection.selectProduct("openSUSE Tumbleweed");
-            }
-            // Check if configuration procedure is progressing
-            await page.locator("::-p-text(Configuring the product)").wait();
+        await productselection.selectProduct(product);
 
-            // refreshing the repositories might take long time
-            await page.locator("h3::-p-text('Overview')").setTimeout(timeout).wait();
-        }
+        // Check if configuration procedure is progressing
+        await page.locator("::-p-text(Configuring the product)").wait();
+        // refreshing the repositories might take long time
+        await page.locator("h3::-p-text('Overview')").setTimeout(timeout).wait();
     });
 }
